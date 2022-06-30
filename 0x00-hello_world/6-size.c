66b2eace72b492876b771bee14566942b37dcfac
nigat22keku <nigatuabebe8@gmail.com>
2022-06-30 06:17:05 -0700

    task6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,6 @@ long int b;
 long long int c;
 char d;
 float f;
-<<<<<<< HEAD
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));

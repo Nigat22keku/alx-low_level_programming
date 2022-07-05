ded92e96086a71973bf6f799db273b76de7a47d5
nigat22keku <nigatuabebe8@gmail.com>
2022-07-05 05:26:44 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
-/*
-* File: 1-alphabet.c
-* Auth:Keku
-*/
 #include "main.h"
-
 /**
-*
-* print_alphabet - show the Alphabet in lowercase, followed by a new line.
+*main - Print _putchar
+*Return: Always 0(Success)
 */
-void print_alphabet(void)
+int main(void)
 {
-char alpha;
-
-for (alpha = 97; alpha <= 122; alpha++)
-_putchar(alpha);
+_putchar(95);
+_putchar(112);
+_putchar(117);
+_putchar(116);
+_putchar(99);
+_putchar(104);
+_putchar(97);
+_putchar(114);
 _putchar('\n');
+return (0);
 }

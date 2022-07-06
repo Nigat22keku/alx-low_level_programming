32d5d23f716ea53d8a83d0fc36a13100d110c846
nigat22keku <nigatuabebe8@gmail.com>
2022-07-06 05:56:52 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
 #include"<main.h"
 
 /**
-*_putchar -check if a character is lowercase.
-*@c: The character to be checked.
+* main - print putchar
 *Return: Always 0(Success)
 */
-int _putchar(char c)
+int main(void)
 {
 _putchar('_');
-_putchar(112);
-_putchar(117);
-_putchar(116);
-_putchar(99);
-_putchar(104);
-_putchar(97);
-_putchar(114);
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 
 return (0);

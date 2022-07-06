8ed4c776190717738303daccdcee23e440978bea
nigat22keku <nigatuabebe8@gmail.com>
2022-07-06 05:22:31 -0700

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,12 @@
  */
 #include "main.h"
 /**
-* keku 
-*/                                                                                                                                   
-int _islower(int c)                                                                                                                     {
-
+* kek
+*/
+int _islower(int c)
+{
 if (c >= 97 && c <= 122)
-return (1);                                                                                                                             
+return (1);
 else
 return (0);
 }

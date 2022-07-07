cddb0259825e91d5d9745a603992bf19f42dac56
nigat22keku <nigatuabebe8@gmail.com>
2022-07-07 04:47:37 -0700

    1 task

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-nt lagest_number(int a, int b, int c);
+int lagest_number(int a, int b, int c);
 viod print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 viod print_times_table(int n);

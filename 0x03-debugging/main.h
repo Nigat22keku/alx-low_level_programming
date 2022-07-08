5c9b99a7d117c7b0c499f0987503336e26671d46
nigat22keku <nigatuabebe8@gmail.com>
2022-07-07 23:37:47 -0700

    task0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,10 +4,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int lagest_number(int a, int b, int c);
-viod print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
-viod print_times_table(int n);
+
 
 
 #endif /* MAIN_H */

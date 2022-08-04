13c1739bfb7ebe0d50d2a1cbcff630a638b522d9
nigat22keku <nigatuabebe8@gmail.com>
2022-08-04 05:55:07 -0700

    hot fix

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,20 +16,14 @@ if (argc != 4)
 printf("Error\n");
 exit(98);
 }
-
-if (
-(strcmp(argv[2], "+") &&
- strcmp(argv[2], "-") &&
- strcmp(argv[2], "*") &&
- strcmp(argv[2], "/") &&
- strcmp(argv[2], "%")
- )
+if ((strcmp(argv[2], "+") && strcmp(argv[2], "-")
+&& strcmp(argv[2], "*") && strcmp(argv[2], "/") && strcmp(argv[2], "%"))
 {
 printf("Error\n");
 exit(99);
 }
 if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
-	&& atoi(argv[3]) == 0)
+&& atoi(argv[3]) == 0)
 {
 printf("Error\n");
 exit(100);

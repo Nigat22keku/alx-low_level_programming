07b9ebe0c53850480e5c7f6b120e3c817552d409
nigat22keku <nigatuabebe8@gmail.com>
2022-08-18 04:05:17 -0700

    hot fix

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,10 +12,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int t = 0;
 	unsigned long int q = n ^ m;
-	while (q)
-	{
-		t += (q & 1);
-		(q >>= 1);
-	}
-	return (t);
+
+while (q)
+{
+t += (q & 1);
+(q >>= 1);
+}
+return (t);
 }
